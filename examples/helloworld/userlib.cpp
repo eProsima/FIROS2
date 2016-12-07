48b50724569c550ca45412e900f3a6a1176a2385
Javier Isabel <JavierIsabel@EPROSIMA.COM>
2016-12-07 11:42:15 +0100

    fix example for Windows (again)

--- examples/helloworld/userlib.cpp
+++ examples/helloworld/userlib.cpp
@@ -3,22 +3,22 @@
 #include "HelloWorldPubSubTypes.h"
 
 #if defined(_WIN32) && defined (BUILD_SHARED_LIBS)
-	#if defined (_MSC_VER)
-		#pragma warning(disable: 4251)
-	#endif
-  #if defined(testlib_EXPORTS)
-  	#define  USER_LIB_EXPORT __declspec(dllexport)
-  #else
-    #define  USER_LIB_EXPORT __declspec(dllimport)
-  #endif
+#if defined (_MSC_VER)
+#pragma warning(disable: 4251)
+#endif
+#if defined(userlib_EXPORTS)
+#define  USER_LIB_EXPORT __declspec(dllexport)
 #else
-  #define USER_LIB_EXPORT
+#define  USER_LIB_EXPORT __declspec(dllimport)
+#endif
+#else
+#define USER_LIB_EXPORT
 #endif
 
 extern "C" void USER_LIB_EXPORT transform(SerializedPayload_t *serialized_input, SerializedPayload_t *serialized_output){
 	// User types
-    std_msgs::msg::dds_::String_ string_data;
-    std_msgs::msg::dds_::String_PubSubType string_pst;
+	std_msgs::msg::dds_::String_ string_data;
+	std_msgs::msg::dds_::String_PubSubType string_pst;
 	HelloWorld helloworld_data;
 	HelloWorldPubSubType helloworld_pst;
 
@@ -26,7 +26,7 @@ extern "C" void USER_LIB_EXPORT transform(SerializedPayload_t *serialized_input,
 	helloworld_pst.deserialize(serialized_input, &helloworld_data);
 
 	// Custom transformation
-    string_data.data_(helloworld_data.message() + ": " + std::to_string(helloworld_data.index()));
+	string_data.data_(helloworld_data.message() + ": " + std::to_string(helloworld_data.index()));
 	std::cout << string_data.data_() << std::endl;
 
 	// Serialization
